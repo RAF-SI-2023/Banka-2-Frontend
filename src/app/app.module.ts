b6862c1016e868c9e5e09ef23554d3999b07f822
Milos Vasiljevic <mvasiljevic8519rn@raf.rs>
2024-03-09 21:57:44 +0100

    navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
@@ -10,6 +15,9 @@ import { UserpanelComponent } from './components/userpanel/userpanel.component';
 import { AdminpanelComponent } from './components/adminpanel/adminpanel.component';
 import { CreateBankAccountComponent } from './components/create-bank-account/create-bank-account.component';
 import { CreateBankProfileComponent } from './components/create-bank-profile/create-bank-profile.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -20,11 +28,16 @@ import { CreateBankProfileComponent } from './components/create-bank-profile/cre
     UserpanelComponent,
     AdminpanelComponent,
     CreateBankAccountComponent,
-    CreateBankProfileComponent
+    CreateBankProfileComponent,
+    NavigationMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
