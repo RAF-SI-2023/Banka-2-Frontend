7596e66e577c4128915094fca8da0a5ec26a01f3
kristinakatiic <kkatic8421rn@raf.rs>
2024-03-11 18:57:12 +0100

    Dodata logika za prikaz forme za promenu lozinke korisnika

--- src/app/components/userpanel/userpanel.component.ts
+++ src/app/components/userpanel/userpanel.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./userpanel.component.css']
 })
 export class UserpanelComponent {
+  users: any[] = [
+    { id: 1, firstName: 'John', lastName: 'Doe', email: 'john@example.com' },
+    { id: 2, firstName: 'Jane', lastName: 'Doe', email: 'jane@example.com' }
+  ];
 
+  showPasswordForm: boolean = false;
+  newPassword: string = '';
+  confirmPassword: string = '';
+  selectedUser: any;
+
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'changePassword'];
+
+  showChangePasswordForm(user: any) {
+    this.selectedUser = user;
+    this.showPasswordForm = true;
+  }
+
+  changePassword() {
+    if (this.newPassword === this.confirmPassword) {
+      console.log(‘Password changed for user:’, this.selectedUser.firstName);
+      this.showPasswordForm = false;
+      this.newPassword = '';
+      this.confirmPassword = '';
+    } else {
+      alert(‘Passwords do not match. Enter the same password in both fields, please.’);
+    }
+  }
 }
