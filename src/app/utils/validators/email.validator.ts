8b18bdb2d83df5e3774026a367101787b3810dd0
Aleksa0308 <aleksas0308@gmail.com>
2024-03-11 14:12:07 +0100

    feat: add custom email validator

---
+++ src/app/utils/validators/email.validator.ts
@@ -0,0 +1,16 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
+
+export const emailValidator = (): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+
+    if (!value) {
+      return null;
+    }
+
+    const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    const isValid = pattern.test(value);
+
+    return isValid ? null : {invalidEmail: true};
+  };
+}
