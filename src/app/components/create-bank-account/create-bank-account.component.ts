7a1154d658c479dee3be8b3b7f65b3cefad09572
marija <mgajtanovic8521rn@raf.rs>
2024-03-11 11:13:03 +0100

    Implemented HTML/CSS code for the bank account creation page. Included toolbar from home page. Created form with partial data entry fields. Added button for "submit" forms. Implemented "submit" button functionality, which sends form data to the backend (simulated in my case). Also added redirection to Admin Panel after successful bank account creation.

--- src/app/components/create-bank-account/create-bank-account.component.ts
+++ src/app/components/create-bank-account/create-bank-account.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-bank-account',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-bank-account.component.css']
 })
 export class CreateBankAccountComponent {
+
+  name: string = '';
+  lastName: string = '';
+  email: string = '';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  submitForm() {
+    const formData = {
+      name: this.name,
+      lastName: this.lastName,
+      email: this.email
+    };
+    // Simulation of sending data to the backend
+    const backendUrl = ''; // waiting for backend URL
+    this.http.post<any>(backendUrl, formData).subscribe(
+      (response) => {
+        console.log('Data successfully sent to backend.', response);
+        // Add a redirect to the Admin Panel
+        this.router.navigate(['/adminpanel']);
+
+      },
+      (error) => {
+        console.error('Error sending data to backend.', error);
+
+      }
+    );
+  }
 
 }
