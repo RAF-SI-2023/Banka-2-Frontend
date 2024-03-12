2922d7918d393598e7085db0a8491cefdcaebc3b
Aleksa0308 <aleksas0308@gmail.com>
2024-03-12 23:50:34 +0100

    feat: add bank account creation forms

---
+++ src/app/components/create-bank-account/foreign-form/foreign-form.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForeignFormComponent } from './foreign-form.component';
+
+describe('ForeignFormComponent', () => {
+  let component: ForeignFormComponent;
+  let fixture: ComponentFixture<ForeignFormComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ForeignFormComponent]
+    });
+    fixture = TestBed.createComponent(ForeignFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
