59810ed3a14d23703d062e70ba96c3e86f03749f
Aleksa0308 <aleksas0308@gmail.com>
2024-03-21 15:02:50 +0100

    fix: display data for different user types

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  iAmServiceApiUrl: 'http://192.168.1.3:8000/api',
+  iAmServiceApiUrl: 'http://localhost:8000/api',
   StockServiceApiUrl: 'http://192.168.1.3:8001/api',
   notificationServiceApiUrl: 'http://192.168.1.3:8002/api',
   bankServiceApiUrl: 'http://192.168.1.3:8003/api',
