33609dd21247b098478891be2788b61a785e8be2
Andrej Đenadić <adjenadic@pm.me>
2024-03-26 15:10:53 +0100

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+	production: true,
+	iamServiceApiUrl: 'http://localhost:8000/api',
+	stockServiceApiUrl: 'http://localhost:8001/api',
+	notificationServiceApiUrl: 'http://localhost:8002/api',
+	bankServiceApiUrl: 'http://localhost:8003/api',
+};
