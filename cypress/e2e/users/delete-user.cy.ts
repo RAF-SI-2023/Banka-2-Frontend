01a8d56d3f4e96099a94b17f4d0550e706bc9983
Aleksa0308 <aleksas0308@gmail.com>
2024-04-10 15:04:27 +0200

    feat: router test cases

--- cypress/e2e/users/delete-user.cy.ts
+++ cypress/e2e/users/delete-user.cy.ts
@@ -7,15 +7,15 @@ describe('Deleting Employee', () => {
 	it('should delete an employee when "Obriši" button is clicked', () => {
 		// Assuming you have some users listed in a table
 		// Locate the user row you want to delete and click the "Obriši" button
-		cy.get('table').contains('tr', 'test@example.com').within(() => {
+		cy.get('table').contains('tr', 'USER').within(() => {
 			// click the row where the user is located so that the row is selected
-			// select the specific cell in the row
-			cy.get('td').contains('test@example.com').click();
+			// select the specific cell in the row that containt USER or EMPLOYEE
+			cy.get('td').contains('USER').click();
 		});
 		cy.get('[data-test="delete-button"]').click();
 
 		// After clicking the delete button, verify that the user is deleted
 		// For example, check if the user's data is no longer present in the table
-		cy.get('table').should('not.contain', 'test@example.com');
+		cy.contains('Zahtev je uspešan!').should('exist');
 	});
 });
