909b3425cc6a4307b149fee6bb7b529620d3d9d8
Aleksa0308 <aleksas0308@gmail.com>
2024-04-10 12:28:17 +0200

    feat: login cypress test

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,37 @@
+describe('Login Component', () => {
+	beforeEach(() => {
+		cy.visit('/login'); // Assuming your login page is accessible at /login route
+	});
+
+	// Check if the login form is displayed
+	it('should display login form', () => {
+		cy.get('form').should('exist');
+		cy.get('input[type="email"]').should('exist');
+		cy.get('input[type="password"]').should('exist');
+		cy.get('button[type="submit"]').should('exist').contains('Uloguj se');
+	});
+
+	// Check if email validation works
+	it('should show error message for invalid credentials', () => {
+		cy.get('input[type="email"]').type('invalidemail');
+		cy.get('input[type="password"]').type('invalidpassword');
+		cy.get('button[type="submit"]').click();
+		cy.contains('Pogrešan format email-a.').should('exist');
+	});
+
+	// Check if invalid credentials show error message
+	it('should show error message for wrong credentials', () => {
+		cy.get('input[type="email"]').type('invalidemail@example.com');
+		cy.get('input[type="password"]').type('invalidpassword');
+		cy.get('button[type="submit"]').click();
+		cy.contains('Neautorizovan pristup!').should('exist');
+	});
+
+	// Successful login test
+	it('should login with valid credentials', () => {
+		cy.get('input[type="email"]').type('vasa_email_adresa_1@gmail.com');
+		cy.get('input[type="password"]').type('admin');
+		cy.get('button[type="submit"]').click();
+		cy.url().should('include', '/home');
+	});
+});
--- cypress/e2e/router.cy.ts
+++ cypress/e2e/router.cy.ts
@@ -1,17 +1,16 @@
-const baseUrl = 'http://localhost:4200'; // Set the base URL for your Angular application
 
 describe('Router', () => {
 	it('Navigates to Landing page', () => {
-		cy.visit(baseUrl); // Visit the base URL
+		cy.visit('/'); // Visit the base URL
 	});
 
 	it('Navigates to Login page', () => {
-		cy.visit(`${baseUrl}/login`); // Visit the login route directly
+		cy.visit(`/login`); // Visit the login route directly
 		cy.url().should('include', '/login'); // Check if the URL contains '/login'
 	});
 
 	it('Navigates to Create Bank Profile page', () => {
-		cy.visit(`${baseUrl}/create-bank-profile`); // Visit the create-bank-profile route directly
+		cy.visit(`/create-bank-profile`); // Visit the create-bank-profile route directly
 		cy.url().should('include', '/create-bank-profile'); // Check if the URL contains '/create-bank-profile'
 	});
 });
