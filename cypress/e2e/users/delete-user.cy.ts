b2ba464f5a153a6b9d2c7acf619de2498a3c7ecf
Aleksa0308 <aleksas0308@gmail.com>
2024-04-10 14:54:44 +0200

    feat: delete user test

---
+++ cypress/e2e/users/delete-user.cy.ts
@@ -0,0 +1,21 @@
+describe('Deleting Employee', () => {
+	beforeEach(() => {
+		cy.login('vasa_email_adresa_1@gmail.com', 'admin'); // Login as Admin
+		cy.visit('/users'); // Assuming your users component is accessible at /users route
+	});
+
+	it('should delete an employee when "Obriši" button is clicked', () => {
+		// Assuming you have some users listed in a table
+		// Locate the user row you want to delete and click the "Obriši" button
+		cy.get('table').contains('tr', 'test@example.com').within(() => {
+			// click the row where the user is located so that the row is selected
+			// select the specific cell in the row
+			cy.get('td').contains('test@example.com').click();
+		});
+		cy.get('[data-test="delete-button"]').click();
+
+		// After clicking the delete button, verify that the user is deleted
+		// For example, check if the user's data is no longer present in the table
+		cy.get('table').should('not.contain', 'test@example.com');
+	});
+});
