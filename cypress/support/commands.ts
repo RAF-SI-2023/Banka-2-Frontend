f368a8fde2423b866752cc1d1535c988bd10bed4
Aleksa0308 <aleksas0308@gmail.com>
2024-04-10 14:07:30 +0200

    feat: add employee test and login command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,18 @@
 //     }
 //   }
 // }
+
+
+declare namespace Cypress {
+	interface Chainable {
+		login(email: string, password: string): Chainable;
+	}
+}
+
+Cypress.Commands.add('login', (email: string, password: string) => {
+	cy.visit('/login');
+	cy.get('input[type="email"]').type(email);
+	cy.get('input[type="password"]').type(password);
+	cy.get('button[type="submit"]').click();
+	cy.url().should('include', '/home');
+});
