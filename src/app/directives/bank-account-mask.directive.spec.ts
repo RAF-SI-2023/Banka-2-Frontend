2e8cba97f401b1ef8391cef58ea479f9c8077fc7
Andrej Đenadić <adjenadic@pm.me>
2024-04-12 20:37:34 +0200

    bank-account-mask.directive.spec.ts fix

--- src/app/directives/bank-account-mask.directive.spec.ts
+++ src/app/directives/bank-account-mask.directive.spec.ts
@@ -1,8 +1,40 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { BankAccountMaskDirective } from './bank-account-mask.directive';
+import { Component, DebugElement } from '@angular/core';
+
+@Component({
+	template: `<input
+		type="text"
+		appBankAccountMask
+	/>`,
+})
+class TestComponent {}
 
 describe('BankAccountMaskDirective', () => {
-	it('should create an instance', () => {
-		const directive = new BankAccountMaskDirective();
-		expect(directive).toBeTruthy();
+	let component: TestComponent;
+	let fixture: ComponentFixture<TestComponent>;
+	let inputEl: DebugElement;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [TestComponent, BankAccountMaskDirective],
+		}).compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(TestComponent);
+		component = fixture.componentInstance;
+		inputEl = fixture.debugElement.query(By.css('input'));
+		fixture.detectChanges();
+	});
+
+	it('should apply the bank account mask', () => {
+		// Simulate user input
+		inputEl.nativeElement.value = '1234567890123456';
+		inputEl.triggerEventHandler('input', { target: inputEl.nativeElement });
+
+		// Ensure that the input value is properly masked
+		expect(inputEl.nativeElement.value).equal('123-4567-890123456');
 	});
 });
