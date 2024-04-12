466c84c38b0b421dfe3724e3aa1273153030ce1a
Andrej Đenadić <adjenadic@pm.me>
2024-04-12 20:29:33 +0200

    prow

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:4200',
+		screenshotOnRunFailure: true,
+		screenshotsFolder: 'cypress/screenshots',
+		video: true,
+		videosFolder: 'cypress/videos',
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
