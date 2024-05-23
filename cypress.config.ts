5792d4e30826c5ac01a6fbb1de5be50206ed485e
Andrej Đenadić <adjenadic@pm.me>
2024-05-23 22:18:51 +0200

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
-		baseUrl: 'http://localhost:80',
+		baseUrl: 'http://localhost:4200',
 		screenshotOnRunFailure: true,
 		screenshotsFolder: 'cypress/screenshots',
 		setupNodeEvents(on, config) {
