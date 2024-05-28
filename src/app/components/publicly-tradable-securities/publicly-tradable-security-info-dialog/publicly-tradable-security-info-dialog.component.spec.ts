e04c8cd02dc00e18031df779033a1d45e3579143
Andrej Đenadić <adjenadic@pm.me>
2024-05-28 02:21:20 +0200

    SecurityDto fix

---
+++ src/app/components/publicly-tradable-securities/publicly-tradable-security-info-dialog/publicly-tradable-security-info-dialog.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PubliclyTradableSecurityInfoDialogComponent } from './publicly-tradable-security-info-dialog.component';
+
+describe('PubliclyTradableSecurityInfoDialogComponent', () => {
+	let component: PubliclyTradableSecurityInfoDialogComponent;
+	let fixture: ComponentFixture<PubliclyTradableSecurityInfoDialogComponent>;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [PubliclyTradableSecurityInfoDialogComponent],
+		});
+		fixture = TestBed.createComponent(PubliclyTradableSecurityInfoDialogComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
