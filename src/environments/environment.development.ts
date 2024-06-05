2754eda9150a1bfd381f02eacda96303ab087748
Milos Vasiljevic <mvasiljevic8519rn@raf.rs>
2024-06-05 02:27:13 +0200

    env u dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,9 @@
 export const environment = {
-	production: false,
-	iamServiceApiUrl: 'http://localhost:8000/api',
-	stockServiceApiUrl: 'http://localhost:8001/api',
-	notificationServiceApiUrl: 'http://localhost:8002/api',
-	bankServiceApiUrl: 'http://localhost:8003/api',
-	otcServiceApiUrl: 'http://localhost:8004/api',
+    production: false,
+    iamServiceApiUrl: 'https://banka-2-dev.si.raf.edu.rs/iam-service/api',
+    stockServiceApiUrl: 'https://banka-2-dev.si.raf.edu.rs/stock-service/api',
+    notificationServiceApiUrl:
+        'https://banka-2-dev.si.raf.edu.rs/notification-service/api',
+    bankServiceApiUrl: 'https://banka-2-dev.si.raf.edu.rs/bank-service/api',
+    otcServiceApiUrl: 'https://banka-2-dev.si.raf.edu.rs/otc-service/api',
 };
