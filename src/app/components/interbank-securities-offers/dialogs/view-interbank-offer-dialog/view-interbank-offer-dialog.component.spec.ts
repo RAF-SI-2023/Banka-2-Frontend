bb34721e03f854c81a3c4a23e743475a14b2c0bf
Milos Vasiljevic <72025505+mvasiljevic8519rn@users.noreply.github.com>
2024-06-29 19:49:31 +0200

    refresh btn, view dialog 2x, (#221)

---
+++ src/app/components/interbank-securities-offers/dialogs/view-interbank-offer-dialog/view-interbank-offer-dialog.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewInterbankOfferDialogComponent } from './view-interbank-offer-dialog.component';
+
+describe('ViewInterbankOfferDialogComponent', () => {
+  let component: ViewInterbankOfferDialogComponent;
+  let fixture: ComponentFixture<ViewInterbankOfferDialogComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ViewInterbankOfferDialogComponent]
+    });
+    fixture = TestBed.createComponent(ViewInterbankOfferDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
